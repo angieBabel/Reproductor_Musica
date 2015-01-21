4ee0fa34f3a84d65b75e71f32b4c545571da0ad2
Angelica Barreda B <anjelyk.b@gmail.com>
2015-01-21 13:46:00 -0600

    Revert "Prueba"

--- routes/media.js
+++ routes/media.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/media', function(req, res) {
-  res.render('Prueba', { title: 'Media Player' });
+router.get('/', function(req, res) {
+  res.render('Media', { title: 'Media Player' });
 });
 
 module.exports = router;
