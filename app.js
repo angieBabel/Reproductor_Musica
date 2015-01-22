98fe3711c1f7a772e08769aa249d7d6f7ef201f1
Noel Alejandro <noel_ska15@hotmail.com>
2015-01-21 18:38:30 -0600

    Installed everyauth module and dependencies

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var everyauth = require('everyauth');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -17,15 +19,39 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(session({ secret: 'secrit cat' }));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(everyauth.middleware(app));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/media/', media);
+
+everyauth.google
+  .entryPath('/auth/google')
+  .callbackPath('/auth/google/callback')
+  .appId('246536898483-66qbs3lg8a6gt3p2atqh7i0r7ntd7t07.apps.googleusercontent.com')
+  .appSecret('tfvNon7QVgwEGlGiQRtZoaKQ')
+  .scope('https://www.google.com/m8/feeds') // What you want access to
+  .handleAuthCallbackError( function (req, res) {
+    // If a user denies your app, Google will redirect the user to
+    // /auth/facebook/callback?error=access_denied
+    // This configurable route handler defines how you want to respond to
+    // that.
+    // If you do not configure this, everyauth renders a default fallback
+    // view notifying the user that their authentication failed and why.
+  })
+  .findOrCreateUser( function (session, accessToken, accessTokenExtra, googleUserMetadata) {
+    // find or create user logic goes here
+    // Return a user or Promise that promises a user
+    // Promises are created via
+    //     var promise = this.Promise();
+  })
+  .redirectPath('/');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
